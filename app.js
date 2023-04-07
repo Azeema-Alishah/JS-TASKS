b2f80d7f25558a7731a1dad9aaa939b3211ebf0f
FIRST_NAME LAST_NAME <azeemaa.abid@gmail.com>
2023-04-07 10:03:03 -0700

    FEAT:Initialize Project

---
+++ app.js
@@ -0,0 +1,5 @@
+var ZakaatPercentage = 0.025
+var num = prompt("Enter number to calculate Zakaat: ");
+var result  = ZakaatPercentage * Number(num);
+console.log(result)
+alert("Your Zakaat value is: "+ result);
