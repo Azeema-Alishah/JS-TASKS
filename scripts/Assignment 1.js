4d93ef528c257add2bc22eaa8ae9161f991bbeb1
FIRST_NAME LAST_NAME <azeemaa.abid@gmail.com>
2023-04-14 12:26:40 -0700

    tasks 5-8

---
+++ scripts/Assignment 1.js
@@ -0,0 +1,99 @@
+//                [BQ TASKS]
+
+                     //       TASK 1 ZAKAAT       //
+
+//var ZakaatPercentage = 0.025
+//var userInput = prompt("Enter number to calculate Zakaat: ");
+// var result  = ZakaatPercentage * Number(userInput);
+//console.log(result)
+//alert("Your Zakaat value is: "+ result);
+          
+          
+                     //       TASK 2 FITRA      //
+          
+//var wheat = "Wheat"
+//var raisin = "Raisin"          
+//var oats = "Oats"          
+//var dates = "Dates"          
+//var familyMembers = Number(prompt("Total Family Members:"))
+//var fitra_method = prompt("Choose fitra method from: Wheat = 250Rs , Oats = 450Rs , Dates = 2100Rs or Raisin = 2800Rs")
+ //if(fitra_method == wheat){
+              // alert("Your fitra amount is "+ 250*familyMembers)
+ //} else if(fitra_method == oats){
+              // alert("Your fitra amount is "+ 450*familyMembers)
+ //} else if(fitra_method == dates){
+               //alert("Your fitra amount is "+ 2100*familyMembers)
+//} else if(fitra_method == raisin){
+               //alert("Your fitra amount is "+ 2800*familyMembers)
+         //  }
+          
+          
+                     //       TASK 3 Secret No.      //
+          
+//var secretNumber = 3
+//var guessNumber = prompt("Guess the Secret No. from a Range of 1-10:")
+  //         console.log(secretNumber)
+//if ( guessNumber == secretNumber)
+  //             {alert("Congatulations! You have guessed the secret no")}
+          
+//else if (guessNumber > secretNumber )
+  //                {alert("Incorrect! Guess is too high, please try again")}
+          
+//if (guessNumber < secretNumber )
+  //                {alert("Incorrect! Guess is too low, please try again")}
+          
+          
+                    //       TASK 4 Capitalized Case Name      //
+          
+//var name = prompt("Your Name:")
+//let first_letter = name.slice(0,1)
+//alert("Your name with first letter capitalized: "+ first_letter.toUpperCase()+name.slice(1,))
+
+
+                   //     Task 5 Contact Number and Name       //
+
+var contactNumber = []
+var contactNames = []
+var number =Number( prompt("Contact No.:"))
+   for (var i = 0; i <= number; i++)
+var name = prompt("Name:")
+
+
+
+          
+
+
+                  //     Task 6 Array Item Removal       //
+var products = ["Ferarri" , "Mercedes" , "Tesla" , "Lamborghini", "BMW" , "Audi" , "Bugatti"];
+
+
+
+                 //     Task 7 Eligibility Criteria to Vote      //
+var Nationality = prompt('Your Nationality:') 
+if (Nationality== "Pakistani" || Nationality=="Indian") 
+     { 
+          } 
+else {
+    alert('Ineligible to Vote')
+}  
+var Gender = prompt('Your Gender:')
+var Age = Number(prompt('Your Age:') )
+if (Gender == "Male" && Age == ">=18") {
+    alert('Eligible to Vote')
+}
+else (Age == "<18") 
+{
+    alert('Ineligible to Vote')
+}
+
+          
+
+
+                 //     Task 8 PCT for WC      //
+
+var WorldCupSquad = ["Babar Azam" , "Muhammad Rizwan" , "Shadab Khan" , "Shaheen Afridi" ,
+                     "Haris Rauf" , "Naseem Shah" , "Imaad Wasim" , "Shoaib Malik" ,
+                     "Muhammad Hasnain" , "Iftikhar Ahmad" , "Muhammad Haris" , 
+                     "Sarfaraz Ahmad" , "Fakhar Zaman" , "Saaim Ayub" , "Haris Sohail"
+]
+
--- scripts/app.js
+++ scripts/app.js
@@ -2,52 +2,52 @@
 
            //       TASK 1 ZAKAAT       //
 
- var ZakaatPercentage = 0.025
- var userInput = prompt("Enter number to calculate Zakaat: ");
- var result  = ZakaatPercentage * Number(userInput);
- console.log(result)
- alert("Your Zakaat value is: "+ result);
+ //var ZakaatPercentage = 0.025
+ //var userInput = prompt("Enter number to calculate Zakaat: ");
+ //var result  = ZakaatPercentage * Number(userInput);
+ //console.log(result)
+ //alert("Your Zakaat value is: "+ result);
 
 
            //       TASK 2 FITRA      //
 
- var wheat = "Wheat"
- var raisin = "Raisin"          
- var oats = "Oats"          
- var dates = "Dates"          
- var familyMembers = Number(prompt("Total Family Members:"))
- var fitra_method = prompt("Choose fitra method from: Wheat = 250Rs , Oats = 450Rs , Dates = 2100Rs or Raisin = 2800Rs")
- if(fitra_method == wheat){
-     alert("Your fitra amount is "+ 250*familyMembers)
- } else if(fitra_method == oats){
-     alert("Your fitra amount is "+ 450*familyMembers)
- } else if(fitra_method == dates){
-     alert("Your fitra amount is "+ 2100*familyMembers)
- } else if(fitra_method == raisin){
-     alert("Your fitra amount is "+ 2800*familyMembers)
- }
+ //var wheat = "Wheat"
+ //var raisin = "Raisin"          
+ //var oats = "Oats"          
+ //var dates = "Dates"          
+ //var familyMembers = Number(prompt("Total Family Members:"))
+ //var fitra_method = prompt("Choose fitra method from: Wheat = 250Rs , Oats = 450Rs , Dates = 2100Rs or Raisin = 2800Rs")
+ //if(fitra_method == wheat){
+   //  alert("Your fitra amount is "+ 250*familyMembers)
+ //} else if(fitra_method == oats){
+   //  alert("Your fitra amount is "+ 450*familyMembers)
+ //} else if(fitra_method == dates){
+   //  alert("Your fitra amount is "+ 2100*familyMembers)
+ //} else if(fitra_method == raisin){
+   //  alert("Your fitra amount is "+ 2800*familyMembers)
+ //}
 
 
            //       TASK 3 Secret No.      //
 
- var secretNumber = 3
- var guessNumber = prompt("Guess the Secret No. from a Range of 1-10:")
- console.log(secretNumber)
- if ( guessNumber == secretNumber)
-     {alert("Congatulations! You have guessed the secret no")}
+ //var secretNumber = 3
+ //var guessNumber = prompt("Guess the Secret No. from a Range of 1-10:")
+ //console.log(secretNumber)
+ //if ( guessNumber == secretNumber)
+   //  {alert("Congatulations! You have guessed the secret no")}
 
-  else if (guessNumber > secretNumber )
-        {alert("Incorrect! Guess is too high, please try again")}
+  //else if (guessNumber > secretNumber )
+    //    {alert("Incorrect! Guess is too high, please try again")}
 
- if (guessNumber < secretNumber )
-        {alert("Incorrect! Guess is too low, please try again")}
+ //if (guessNumber < secretNumber )
+   //     {alert("Incorrect! Guess is too low, please try again")}
 
 
           //       TASK 4 Capitalized Case Name      //
 
-var name = prompt("Your Name:")
-let first_letter = name.slice(0,1)
-alert("Your name with first letter capitalized: "+ first_letter.toUpperCase()+name.slice(1,))
+//var name = prompt("Your Name:")
+//let first_letter = name.slice(0,1)
+//alert("Your name with first letter capitalized: "+ first_letter.toUpperCase()+name.slice(1,))
 
 
 
